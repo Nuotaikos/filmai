ba120b9956d077cf8b0e4bde3715e0b67e2052f5
Diana <oganauskiene@gmail.com>
2022-07-01 18:46:57 +0300

    select option

--- src/Components/Back/Cats/Create.jsx
+++ src/Components/Back/Cats/Create.jsx
@@ -21,14 +21,15 @@ function Create() {
       <div className="card-body">
         <div className="form-group">
           <label>Movie category</label>
-          <input type="text" className="form-control" onChange={e => setTitle(e.target.value)} value={title} />
-          {/* <select className="form-control" onChange={e => setTitle(e.target.value)} value={title}>
-            <option value="1">Documentary</option>
-            <option value="2">Family</option>
-            <option value="3">Animation</option>
-            <option value="4">Drama</option>
-            <option value="5">Horror</option>
-          </select> */}
+          {/* <input type="text" className="form-control" onChange={e => setTitle(e.target.value)} value={title} /> */}
+          <select className="form-control" onChange={e => setTitle(e.target.value)} value={title}>
+            <option value="Select">Select</option>
+            <option value="Documentary">Documentary</option>
+            <option value="Family">Family</option>
+            <option value="Animation">Animation</option>
+            <option value="Drama">Drama</option>
+            <option value="Horror">Horror</option>
+          </select>
           <small className="form-text text-muted">Select category name here.</small>
         </div>
         <button type="button" className="btn btn-outline-primary" onClick={handleCreate}>Create</button>
