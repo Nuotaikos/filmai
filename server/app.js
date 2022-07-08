3dad39cc874610f264ad72c28d2fc498c01b50ee
Diana <oganauskiene@gmail.com>
2022-07-08 17:09:57 +0300

    Function auth.js

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3003;
 const cors = require("cors");
+const md5 = require('js-md5');
+const uuid = require('uuid');
 app.use(express.json({ limit: '10mb' }));
 app.use(cors());
 const mysql = require("mysql");
@@ -18,6 +20,68 @@ const con = mysql.createConnection({
   password: "",
   database: "filmai",
 });
+
+const doAuth = function (req, res, next) {
+  if (0 === req.url.indexOf('/admin')) {
+    const sql = `
+      SELECT
+      name
+      FROM users
+      WHERE session = ?
+  `;
+    con.query(
+      sql, [req.headers['authorization'] || ''],
+      (err, results) => {
+        if (err) throw err;
+        if (!results.length) {
+          res.status(401).send({});
+          req.connection.destroy();
+        } else {
+          next();
+        }
+      }
+    );
+  } else {
+    next();
+  }
+}
+app.use(doAuth)
+
+// AUTH
+app.get("/login-check", (req, res) => {
+  const sql = `
+  SELECT
+  name
+  FROM users
+  WHERE session = ?
+  `;
+  con.query(sql, [req.headers['authorization'] || ''], (err, result) => {
+    if (err) throw err;
+    if (!result.length) {
+      res.send({ msg: 'error' });
+    } else {
+      res.send({ msg: 'ok' });
+    }
+  });
+});
+
+app.post("/login", (req, res) => {
+  const key = uuid.v4();
+  const sql = `
+  UPDATE users
+  SET session = ?
+  WHERE name = ? AND pass = ?
+`;
+  con.query(sql, [key, req.body.user, md5(req.body.pass)], (err, result) => {
+    if (err) throw err;
+    if (!result.affectedRows) {
+      res.send({ msg: 'error', key: '' });
+    } else {
+      res.send({ msg: 'ok', key });
+    }
+  });
+});
+
 
 // CATEGORIES create
 app.post("/admin/cats", (req, res) => {
