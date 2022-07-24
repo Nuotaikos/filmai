651a0e064ec325b3e99866f796f55c6187f4fe04
Diana <oganauskiene@gmail.com>
2022-07-24 18:49:36 +0300

    front design

--- src/Components/Front/Line.jsx
+++ src/Components/Front/Line.jsx
@@ -6,29 +6,60 @@ function Line({ line }) {
   const { doFilter } = useContext(FrontContext);
 
   return (
-
-    <li className="list-group-item">
-      <div className="item">
-        <div className="content">
-          <div className='mr-4'>
-            {
-              line.photo ? <div className="photo-bin"><img src={line.photo} alt={line.title} /></div> : null
-            }
-          </div>
-          <b>{line.title}</b>
-          <div className="cat" onClick={() => doFilter(line.cid)}>{line.cat}</div>
-          <b className="ml-4">
-            {
-              line.rate_sum ? 'rate: ' + (line.rate_sum / line.rates).toFixed(2) : 'No rates yet'
-            }
-          </b>
-          <i>{line.price.toFixed(2)} Eur</i>
-
-          {/* <span>{['Documentary', 'Family', 'Animation', 'Drama', 'Horror']}</span> */}
-
-        </div>
+    <div class="container text-center">
+      <div class="table-wrapper">
+        <table class="table table-striped table-hover">
+          <thead>
+            <tr>
+              <th className="w-25 p-3 fs-5" scope="col">Photo</th>
+              <th className="fs-5" scope="col">Title</th>
+              <th className="fs-5" scope="col">Category</th>
+              <th className="fs-5" scope="col">Rate</th>
+              <th className="fs-5" scope="col">Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                {
+                  line.photo ? <div className="photo-bin"><img src={line.photo} alt={line.title} /></div> : null
+                }
+              </td>
+              <td>{line.title}</td>
+              <td className="cat text-info" onClick={() => doFilter(line.cid)}>{line.cat}</td>
+              <td>
+                {
+                  line.rate_sum ? 'rate: ' + (line.rate_sum / line.rates).toFixed(2) : 'No rates yet'
+                }
+              </td>
+              <td>{line.price.toFixed(2)} Eur</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-    </li>
+    </div>
+
+
+
+    // <li className="list-group-item">
+    //   <div className="item">
+    //     <div className="content">
+    //       <div className=''>
+    //         {
+    //           line.photo ? <div className="photo-bin"><img src={line.photo} alt={line.title} /></div> : null
+    //         }
+    //       </div>
+    //       <b>{line.title}</b>
+    //       <div className="cat" onClick={() => doFilter(line.cid)}>{line.cat}</div>
+    //       <b className="">
+    //         {
+    //           line.rate_sum ? 'rate: ' + (line.rate_sum / line.rates).toFixed(2) : 'No rates yet'
+    //         }
+    //       </b>
+    //       <i>{line.price.toFixed(2)} Eur</i>
+    //     </div>
+    //   </div>
+    // </li>
   );
 }
 
--- src/Components/Front/Nav.jsx
+++ src/Components/Front/Nav.jsx
@@ -5,7 +5,7 @@ function Nav() {
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <div className="collapse navbar-collapse" id="navbarColor02">
-            WElcom
+            MOVIES RENT
           </div>
         </div>
       </nav>
